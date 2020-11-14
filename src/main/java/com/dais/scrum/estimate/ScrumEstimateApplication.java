40f8d733c91c2eb7da042e0ba8ae93adaf6e9bee
Stephen Maina <stephen.maina@dais.com>
2020-11-14 12:10:44 -0600

    step-1 | initial commit after creating new project

---
+++ src/main/java/com/dais/scrum/estimate/ScrumEstimateApplication.java
@@ -0,0 +1,13 @@
+package com.dais.scrum.estimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumEstimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumEstimateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dais/scrum/estimate/ScrumEstimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dais.scrum.estimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumEstimateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
